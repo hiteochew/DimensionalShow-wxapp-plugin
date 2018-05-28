2fef5936ca941d81c105303ac1524be846c76ed9
jymusic0663 <dong@linyudong.cn>
2018-05-28 14:36:23 +0800

    示例代码

---
+++ 示例代码/pages/index/index.js
@@ -0,0 +1,14 @@
+var plugin = requirePlugin("DimensionalShow");
+Page({
+  data: {
+    picsrcs: [],
+    pics: [
+      "https://~~~~~~/1.jpg",
+      /**序列照片地址集*/
+      "https://~~~~~~/20.jpg/h300"
+    ]
+  },
+
+  onLoad: function () {
+  }
+})
